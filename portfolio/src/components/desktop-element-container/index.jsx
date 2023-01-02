4878c24d6eb44d26a01e3f67a8e4dc8299e15e68
stefan <stefan@stefan.com>
2023-01-02 21:40:29 +0100

    corrected window state

--- portfolio/src/components/desktop-element-container/index.jsx
+++ portfolio/src/components/desktop-element-container/index.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import mycomputer from "../../assets/icons/mycomputer.ico";
 import mydocuments from "../../assets/icons/mydocuments.ico";
 import mynetwork from "../../assets/icons/mynetwork.ico";
@@ -8,36 +7,14 @@ import styles from "./styles.module.css";
 import Window from "../window";
 
 function DesktopContainer() {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => {
-    setOpen(!open);
-  };
   return (
     <section className={styles.desktop_elements}>
-      <DesktopElement
-        onClick={handleOpen}
-        icon={mydocuments}
-        iconTitle={"My Documents"}>
-        {open && <Window close={handleOpen}>my documents</Window>}
+      <DesktopElement icon={mydocuments} iconTitle={"My Documents"}>
+        {<Window></Window>}
       </DesktopElement>
-      <DesktopElement
-        onClick={handleOpen}
-        icon={mycomputer}
-        iconTitle={"My Computer"}>
-        {open && <Window close={handleOpen}>my computer</Window>}
-      </DesktopElement>
-      <DesktopElement
-        onClick={handleOpen}
-        icon={mynetwork}
-        iconTitle={"My Network"}>
-        {open && <Window close={handleOpen}>my network</Window>}
-      </DesktopElement>
-      <DesktopElement
-        onClick={handleOpen}
-        icon={recycle}
-        iconTitle={"Recycle Bin"}>
-        {open && <Window close={handleOpen}>recycle bin</Window>}
-      </DesktopElement>
+      <DesktopElement icon={mycomputer} iconTitle={"My Computer"} />
+      <DesktopElement icon={mynetwork} iconTitle={"My Network"} />
+      <DesktopElement icon={recycle} iconTitle={"Recycle Bin"} />
     </section>
   );
 }
--- portfolio/src/components/window/index.jsx
+++ portfolio/src/components/window/index.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 import Draggable from "react-draggable";
 
 function Window({ children, close }) {
+  const [open, setOpen] = useState(false);
+  function handleClick() {
+    setOpen(!open);
+  }
   return (
-    <div className={styles.container}>
-      <Draggable bounds="parent" handle=".handle">
-        <div className={styles.window}>
-          <div id={styles.top_border} className="handle">
-            <button onClick={close}>X</button>
-          </div>
-          {children}
+    <>
+      <button onClick={handleClick}>open</button>
+
+      {open && (
+        <div className={styles.container}>
+          <Draggable bounds="parent" handle=".handle">
+            <div className={styles.window}>
+              <div id={styles.top_border} className="handle">
+                <button onClick={handleClick}>X</button>
+              </div>
+              {children}
+            </div>
+          </Draggable>
         </div>
-      </Draggable>
-    </div>
+      )}
+    </>
   );
 }
 export default Window;

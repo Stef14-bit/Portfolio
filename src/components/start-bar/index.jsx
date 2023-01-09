64aa3c3d795af675a8e8e6ddb15fc01b50259d6b
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-01-09 20:38:00 +0100

    add content fixed bugs

--- src/components/start-bar/index.jsx
+++ src/components/start-bar/index.jsx
@@ -5,17 +5,20 @@ import { useState } from "react";
 import StartMenu from "../start-menu";
 import DateTime from "../date-time";
 import astronaut from "../../assets/icons/astronaut.bmp";
-import Window from "../window";
+import WindowMobile from "../window-mobile";
 import Iframe from "react-iframe";
 import mycomputer from "../../assets/icons/mycomputer.ico";
 import folder from "../../assets/icons/folder.ico";
 import msn from "../../assets/icons/msn.ico";
+import linkedin from "../../assets/icons/linkedin.png";
 import github from "../../assets/icons/github.png";
 import resume from "../../assets/resume.pdf";
 import internetex from "../../assets/icons/internetex.ico";
 import email from "../../assets/icons/email.ico";
 import morestart from "../../assets/icons/morestart.ico";
 import MinWindow from "../minimized-window";
+import DesktopLink from "../desktop-external-link";
+import Contact from "../contact-form";
 
 function Start() {
   const [open, setOpen] = useState(false);
@@ -26,7 +29,7 @@ function Start() {
   return (
     <div className={styles.start}>
       <button className={styles.start_btn} onClick={handleOpen}>
-        <img src={winlogo} alt="windows logo" />
+        <img src={winlogo} alt="windowMobiles logo" />
         start
       </button>
       {open && (
@@ -35,38 +38,69 @@ function Start() {
           userImg={astronaut}
           userName={"Stefan"}>
           <div className={styles.start_items}>
-            <Window icon={internetex} iconTitle={"Internet Explorer"}>
+            <WindowMobile icon={internetex} iconTitle={"Internet"}>
               <Iframe
                 src="https://www.youtube.com/embed?v=66tfvFeALBQ"
                 frameborder="0"
                 width="100%"
                 height="100%"></Iframe>
-            </Window>
-            <Window icon={email} iconTitle={"Email"}>
+            </WindowMobile>
+
+            <WindowMobile icon={email} iconTitle={"Email"}>
               <h1>Email here</h1>
-            </Window>
+            </WindowMobile>
             <hr className={styles.divider} />
-            <Window icon={mycomputer} iconTitle={"My Computer"}>
+            <WindowMobile icon={mycomputer} iconTitle={"My Computer"}>
               <h1>MY computer</h1>
-            </Window>
-            <Window icon={folder} iconTitle={"My Resume"}>
+            </WindowMobile>
+            <WindowMobile icon={folder} iconTitle={"My Resume"}>
               <Iframe src={resume} width="100%" height="100%" />
-            </Window>
-            <Window icon={msn} iconTitle={"MSN"}>
-              <h1>insert contact form here!!</h1>
-            </Window>
-            <Window icon={github} iconTitle={"GitHub"}>
-              <Iframe
-                src={"https://github.com/Stef14-bit"}
-                width="100%"
-                height="100%"
-              />
-            </Window>
+            </WindowMobile>
+
+            <DesktopLink
+              href={"https://github.com/Stef14-bit"}
+              icon={github}
+              iconTitle={"GitHub"}
+            />
+            <Contact icon={msn} iconTitle={"Contact Me"} />
+
             <hr className={styles.divider} />
             <br />
             <h4>
               More Programs <img src={morestart} alt="more programs" />
             </h4>
+            <hr className={styles.divider} />
+
+            <DesktopLink
+              href={"https://www.linkedin.com/in/stefan-ivanus-941455250/"}
+              icon={linkedin}
+              iconTitle={"LinkedIn"}
+            />
+            <hr className={styles.divider} />
+
+            <WindowMobile icon={folder} iconTitle={"1 Team Project"}>
+              <h2>First team project using HTML CSS and Js</h2>
+
+              <Iframe
+                src={"https://matt0107.github.io/imaginaryCountry/"}
+                width="100%"
+                height="100%"
+              />
+            </WindowMobile>
+            <WindowMobile icon={folder} iconTitle={"2 Team Project"}>
+              <h2>
+                Second team project using Js React and different node packages
+              </h2>
+              <Iframe
+                src={"https://wild-space-explorer.netlify.app/"}
+                width="100%"
+                height="100%"
+              />
+            </WindowMobile>
+            <WindowMobile icon={folder} iconTitle={"3 Team Project"}>
+              <h2>Coming soon ... </h2>
+            </WindowMobile>
+            <hr className={styles.divider} />
           </div>
         </StartMenu>
       )}

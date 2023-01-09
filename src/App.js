dd54ea97f6265c250dd1e6e6814386341408d8e5
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-01-09 18:53:31 +0100

    mobile view try fix 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import Desktop from "./components/desktop";
 import DesktopContainer from "./components/desktop-element-container";
-import Start from "./components/start-bar";
 
 function App() {
   return (
     <div className="App">
       <Desktop />
       <DesktopContainer />
-      <Start />
     </div>
   );
 }
--- src/components/desktop/index.jsx
+++ src/components/desktop/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import bliss from "../../assets/bliss.jpg";
 import styles from "./styles.module.css";
+import Start from "../start-bar";
 
 function Desktop() {
   return (
     <div className={styles.desktop}>
       <img className={styles.background} src={bliss} alt="background" />
+      <Start />
     </div>
   );
 }
